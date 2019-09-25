08ffca04459e53afec1c1fe1f2987e26c2c84359
Joerg-Christian Boehme <joerg@chaosdorf.de>
2019-09-25 17:07:46 +0200

    Try border layout

--- vpnconfig.cpp
+++ vpnconfig.cpp
@@ -3,6 +3,7 @@
 #include <Wt/WContainerWidget.h>
 #include <Wt/WSelectionBox.h>
 #include <Wt/WPushButton.h>
+#include <Wt/WBorderLayout.h>
 
 namespace wuci {
 
@@ -12,14 +13,16 @@ namespace wuci {
 
   std::unique_ptr<Wt::WWidget> VpnConfig::preparePage()
   {
-    auto container = std::make_unique<Wt::WContainerWidget>();
-    auto selectionBox = container->addNew<Wt::WSelectionBox>();
+    auto rootContainer = std::make_unique<Wt::WContainerWidget>();
+    auto container = rootContainer->setLayout(std::make_unique<Wt::WBorderLayout>());
+    container->setSpacing(20);
+    auto selectionBox = container->addWidget(std::make_unique<Wt::WSelectionBox>(), Wt::LayoutPosition::Center);
 
     selectionBox->addItem("Home");
     selectionBox->addItem("Office");
     selectionBox->addItem("Outside");
 
-    ok_ = container->addNew<Wt::WPushButton>("Ok");
+    ok_ = container->addWidget(std::make_unique<Wt::WPushButton>("Ok"), Wt::LayoutPosition::South);
     ok_->disable();
 
     selectionBox->changed().connect(
@@ -30,7 +33,7 @@ namespace wuci {
           }
       });
 
-    return std::move(container);
+    return std::move(rootContainer);
   }
 
   void VpnConfig::connect(std::function<void()> callback)
